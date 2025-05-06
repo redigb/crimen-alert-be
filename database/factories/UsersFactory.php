2e7638c04fd41d3cac76934d2b462993a83fb4b8
redigb <redrojofuego77foreve@gmail.com>
2025-05-06 08:44:04 -0500

    docker config doker local

--- database/factories/UsersFactory.php
+++ database/factories/UsersFactory.php
@@ -2,14 +2,16 @@
 
 namespace Database\Factories;
 
+
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Users>
  */
-class UserFactory extends Factory
+
+class UsersFactory extends Factory
 {
     /**
      * The current password being used by the factory.
@@ -26,7 +28,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            //'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
